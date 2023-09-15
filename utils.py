069cb13a08881b0e0f86848e622c9b7d6553331c
dpatchigolla <dileep.patchigolla@gmail.com>
2023-09-16 00:32:15 +0530

    Added exception handling for materialising past data

--- utils.py
+++ utils.py
@@ -366,14 +366,18 @@ def materialise_past_data(features_valid_time: str, feature_package_path: str, o
     Example Usage:
         materialise_past_data("2022-01-01", "packages/feature_table/models/shopify_user_features", "output/path")
     """
-    path_components = output_path.split(os.path.sep)
-    output_index = path_components.index('output')
-    pb_proj_dir = os.path.sep.join(path_components[:output_index])
-    features_valid_time_unix = int(datetime.strptime(features_valid_time, "%Y-%m-%d").replace(tzinfo=timezone.utc).timestamp())
-    args = ["pb", "run", "-p", pb_proj_dir, "-m", feature_package_path, "--migrate_on_load=True", "--end_time", str(features_valid_time_unix)]
-    print(f"Running following pb command for the date {features_valid_time}: {' '.join(args)} ")
-    #subprocess.run(["pb", "run", "-m", "packages/feature_table/models/shopify_user_features", "--end_time", str(features_valid_time_unix)])
-    subprocess.run(["pb", "run", "-p", pb_proj_dir, "-m", feature_package_path, "--migrate_on_load=True", "--end_time", str(features_valid_time_unix)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    try:
+        path_components = output_path.split(os.path.sep)
+        output_index = path_components.index('output')
+        pb_proj_dir = os.path.sep.join(path_components[:output_index])
+        features_valid_time_unix = int(datetime.strptime(features_valid_time, "%Y-%m-%d").replace(tzinfo=timezone.utc).timestamp())
+        args = ["pb", "run", "-p", pb_proj_dir, "-m", feature_package_path, "--migrate_on_load=True", "--end_time", str(features_valid_time_unix)]
+        print(f"Running following pb command for the date {features_valid_time}: {' '.join(args)} ")
+        #subprocess.run(["pb", "run", "-m", "packages/feature_table/models/shopify_user_features", "--end_time", str(features_valid_time_unix)])
+        subprocess.run(["pb", "run", "-p", pb_proj_dir, "-m", feature_package_path, "--migrate_on_load=True", "--end_time", str(features_valid_time_unix)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    except Exception as e:
+        print(f"Exception occured while materialising data for date {features_valid_time} ")
+        print(e)
 
 def get_material_names_(session: snowflake.snowpark.Session,
                        material_table: str, 

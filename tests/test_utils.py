b975c04f90376b7dbaf4faccf08fdbbf09cdc22d
shekhar-rudder <shekhar@rudderstack.com>
2023-11-10 18:10:15 +0530

    chore: add github action for running unit tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,11 +2,6 @@ import unittest
 from utils import *
 
 class TestUtils(unittest.TestCase):
-    def test_get_label_date_ref_case1(self):
-        feature_date = '2022-01-01'
-        self.assertEqual(get_label_date_ref(feature_date, 1), '2022-01-02')
-        self.assertEqual(get_label_date_ref(feature_date, 0), '2022-01-01')
-        self.assertEqual(get_label_date_ref(feature_date, -1), '2021-12-31')
 
     def test_combine_config(self):
         self.assertEqual(combine_config({"a":2, "b":3}, {"a":1}), {"a":1, "b":3})
@@ -29,5 +24,3 @@ class TestUtils(unittest.TestCase):
 
         self.assertEqual(result, expected_result)
 
-if __name__ == '__main__':
-    unittest.main()

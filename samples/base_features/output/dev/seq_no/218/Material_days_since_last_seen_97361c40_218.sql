3dcd44c905b52f08f1eeb6958e8ecbcf01c7918d
Subham Subhasis Sahoo <subhamsubhasis2002@gmail.com>
2023-11-30 15:24:32 +0530

    sample projects

---
+++ samples/base_features/output/dev/seq_no/218/Material_days_since_last_seen_97361c40_218.sql
@@ -0,0 +1,135 @@
+
+	
+		
+   BEGIN
+
+		
+   DROP VIEW IF EXISTS DUMMY_NON_EXISTENT_VIEW;  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+    
+        
+            
+    
+        
+    
+        
+    
+        
+    
+
+        
+    
+
+			
+
+
+
+    
+        
+        
+
+        
+   CREATE OR REPLACE TEMPORARY TABLE MATERIAL_DAYS_SINCE_LAST_SEEN_97361C40_218_INTERNAL_EV_DAYS_SINCE_LAST_SEEN AS (
+            
+                SELECT * FROM (
+            SELECT
+                user_main_id,
+                 datediff(day, date(Material_user_var_table_cfc56553_218.max_timestamp_bw_tracks_pages), date('2023-11-28 16:16:29')) 
+	
+	 AS days_since_last_seen
+            FROM Material_user_var_table_cfc56553_218
+            
+            WHERE user_main_id IS NOT NULL
+            
+            
+        )
+            
+        );  
+
+        
+        
+  
+      
+          
+          
+          
+          /* Join with single entityvar table and replace old table */
+          
+   CREATE OR REPLACE TEMPORARY TABLE MATERIAL_USER_VAR_TABLE_CFC56553_218NEW AS (
+              SELECT *
+              FROM
+              Material_user_var_table_cfc56553_218
+              LEFT JOIN MATERIAL_DAYS_SINCE_LAST_SEEN_97361C40_218_INTERNAL_EV_DAYS_SINCE_LAST_SEEN USING (user_main_id)
+          );  
+          
+
+          
+          
+          DROP TABLE Material_user_var_table_cfc56553_218;
+          
+
+          
+          
+   CREATE OR REPLACE TABLE MATERIAL_USER_VAR_TABLE_CFC56553_218 AS SELECT * FROM Material_user_var_table_cfc56553_218New;  
+
+          
+          
+          DROP TABLE Material_user_var_table_cfc56553_218New;
+          
+      
+  
+
+
+        
+   DROP TABLE IF EXISTS MATERIAL_DAYS_SINCE_LAST_SEEN_97361C40_218_INTERNAL_EV_DAYS_SINCE_LAST_SEEN;  
+
+        /* Handle default setting */
+        
+    
+
+
+
+			
+    
+        
+            
+    
+        
+    
+        
+    
+        
+    
+
+        
+    
+ 
+	
+	END;  
+	

3186ced44fa4427aa7751f003d121ad3ae41d12d
AMBUJ MISHRA <ambuj.mishra.2k12@gmail.com>
2023-12-21 14:36:21 +0530

    correcting config path in unit tests
    Changes to be committed:
    	modified:   tests/unit/defaults.py

--- tests/unit/defaults.py
+++ tests/unit/defaults.py
@@ -5,6 +5,6 @@ from utils import load_yaml
 class TestDefaults(unittest.TestCase):
 
     def test_default_config(self):
-        default_config = load_yaml('../../config/model_configs.yaml')
+        default_config = load_yaml('./config/model_configs.yaml')
         self.assertIsNone(default_config['data']['mode'])
         self.assertEqual(default_config["data"]["task"], "classification")

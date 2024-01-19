c847837557ce865a3dac4a02ec8c5cbd076bde1a
Dileep Patchigolla <dileep.patchigolla@gmail.com>
2024-01-19 12:00:31 +0530

    Added debug logs to understand k8s and modified logger level to debug (#143)
    * Added debug logs to understand k8s and modified logger level to debug
    
    * removed len(df) that's incompatible in snowflake

--- logger.py
+++ logger.py
@@ -5,13 +5,13 @@ import logging
 logger = logging.getLogger("churn_prediction")
 
 # Configure the logger with the desired log level and handlers
-logger.setLevel(logging.WARNING)
+logger.setLevel(logging.DEBUG)
 
 # file_handler = logging.FileHandler("classifier.log")
 # file_handler.setLevel(logging.INFO)
 
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.WARNING)
+console_handler.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 # file_handler.setFormatter(formatter)

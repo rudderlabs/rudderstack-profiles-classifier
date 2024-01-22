87643d2aadb711b8aa07770712f6d2b0e9b2c80f
Subham Subhasis Sahoo <subhamsubhasis2002@gmail.com>
2024-01-23 01:11:05 +0530

    handled entity and label column

--- RedshiftConnector.py
+++ RedshiftConnector.py
@@ -664,8 +664,8 @@ class RedshiftConnector(Connector):
             if label_column not in feature_table.columns:
                 raise Exception(f"Label column {label_column} is not present in the feature table.")
 
-            # Check if feature_table has at least one column apart from label_column
-            if feature_table.shape[1] <= 1:
+            # Check if feature_table has at least one column apart from label_column and entity_column
+            if feature_table.shape[1] < 3:
                 raise Exception(
                     f"Feature table must have at least one column apart from the label column {label_column}."
                 )

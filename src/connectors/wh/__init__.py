fa9b154f598ccdf1d7648a555787bc93de2c32dc
Subham Subhasis Sahoo <subhamsubhasis2002@gmail.com>
2024-01-31 19:10:36 +0530

    merge update

---
+++ src/connectors/wh/__init__.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Module for handling various warehouse connections"""
+
+from typing import Union
+
+from src.wh.redshift_base_connector import RedShiftConnector
+from src.wh.snowflake_base_connector import SnowflakeConnector
+from src.wh.connector_base import connector_classes
+
+# SnowflakeConnector not used currently in profiles_rudderstack
+def ProfilesConnector(config: dict, **kwargs) -> Union[RedShiftConnector, SnowflakeConnector]:
+    """Creates a connector object based on the config provided
+
+    Args:
+        config: A dictionary containing the credentials and database information for the connector.
+        **kwargs: Additional keyword arguments to pass to the connector.
+
+    Returns:
+        ConnectorBase: Connector object.
+
+    Raises:
+        Exception: Connector not found
+    """
+
+    warehouse_type = config.get("type").lower()
+    connector = connector_classes.get(warehouse_type, None)
+    if connector is None:
+        raise Exception(f"Connector {warehouse_type} not found")
+
+    creds = {
+        "user": config.get("user"),
+        "password": config.get("password"),
+        "account_identifier": config.get("account"),
+        "warehouse": config.get("warehouse"),
+        "host": config.get("host"),
+        "port": config.get("port"),
+    }
+
+    if "role" in config:
+        creds["role"] = config.get("role")
+
+    db_config = {
+        "database": config.get("dbname"),
+        "schema": config.get("schema")
+    }
+
+    connector = connector(creds, db_config, **kwargs)
+    return connector
---
+++ src/utils/constants.py
@@ -0,0 +1,36 @@
+MATERIAL_REGISTRY_TABLE_PREFIX = 'MATERIAL_REGISTRY'
+MATERIAL_TABLE_PREFIX = "material_"
+VAR_TABLE_SUFFIX = ["_var_table", "_all_var_table"]
+ENTITY_VAR_MODEL = "entity_var_model"
+MODEL_FILE_NAME = 'classifier.joblib'
+CARDINAL_FEATURE_THRESOLD = 0.01
+MIN_SAMPLES_FOR_TRAINING = 10
+METRICS_TABLE = "TRAINING_METRICS_v4"
+POSITIVE_BOOLEAN_FLAGS = ["1", 1, "TRUE", "True", "true", "T", "t", True, "YES", "Yes", "yes", "Y", "y"]
+PROCESSOR_MODE_PREFERENCE = {"snowflake": ["native-warehouse"], "redshift": ["local", "rudderstack-infra"]}
+LOCAL_STORAGE_DIR = "data"
+SF_LOCAL_STORAGE_DIR = "/tmp"
+rs_dtypes = '{"text": "character varying(65535)", "num": "float", "bool": "bool", "timestamp": "timestamp without time zone"}'
+PB = '/venv/bin/pb'  # Location of pb executable in rudder-sources
+CLASSIFIER_MIN_LABEL_PROPORTION = 0.05
+CLASSIFIER_MAX_LABEL_PROPORTION = 0.95
+REGRESSOR_MIN_LABEL_DISTINCT_VALUES = 3
+REMOTE_DIR = "/home/ec2-user"
+INSTANCE_ID = "i-001c6544decab0fa3"
+EC2_TEMP_OUTPUT_JSON = "train_results.json"
+S3_BUCKET = "ml-usecases-poc-srinivas"
+AWS_REGION_NAME="us-east-1"
+S3_PATH = "jobId/jobRunId/taskRunId"
+SSM_SLEEP_TIME = 5
+K8S_WH_CREDS_KEY="WAREHOUSE_CREDS"
+K8S_MODE="K8S"
+K8S_TIMEOUT_IN_SEC=120
+
+
+from typing import NamedTuple
+
+class TrainTablesInfo(NamedTuple):
+    feature_table_name: str
+    feature_table_date: int
+    label_table_name: str
+    label_table_date: int

a71ec7e266c984df76b92efb6ba34a70f166c854
Subham Subhasis Sahoo <subhamsubhasis2002@gmail.com>
2024-02-07 01:58:30 +0530

    processor update from main

--- src/processors/ProcessorMap.py
+++ src/processors/ProcessorMap.py
@@ -2,8 +2,12 @@ from src.processors.AWSProcessor import AWSProcessor
 from src.processors.LocalProcessor import LocalProcessor
 from src.processors.SnowflakeProcessor import SnowflakeProcessor
 
+LOCAL_MODE = "local"
+WAREHOUSE_MODE = "native-warehouse"
+RUDDERSTACK_MODE = "rudderstack-infra"
+
 processor_mode_map = {
-    "local": LocalProcessor,
-    "native-warehouse": SnowflakeProcessor,
-    "rudderstack-infra": AWSProcessor,
+    LOCAL_MODE: LocalProcessor,
+    WAREHOUSE_MODE: SnowflakeProcessor,
+    RUDDERSTACK_MODE: AWSProcessor,
 }

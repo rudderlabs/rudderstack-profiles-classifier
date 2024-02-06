74cc04e345f1747062092161e1ed9c1bd18e2336
shekhar-rudder <shekhar@rudderstack.com>
2024-02-06 18:19:41 +0530

    update site config test

--- tests/integration/redshift/classifier.py
+++ tests/integration/redshift/classifier.py
@@ -141,7 +141,8 @@ def validate_reports():
 
 def create_site_config_file(creds, siteconfig_path):
     json_data = {
-        "connections": {"test": {"target": "test", "outputs": {"test": creds}}}
+        "connections": {"test": {"target": "test", "outputs": {"test": creds}}},
+        "py_models": {"credentials_presets": None},
     }
     yaml_data = yaml.dump(json_data, default_flow_style=False)
     with open(siteconfig_path, "w") as file:
--- tests/integration/redshift/regressor.py
+++ tests/integration/redshift/regressor.py
@@ -157,7 +157,8 @@ def validate_predictions_df():
 
 def create_site_config_file(creds, siteconfig_path):
     json_data = {
-        "connections": {"test": {"target": "test", "outputs": {"test": creds}}}
+        "connections": {"test": {"target": "test", "outputs": {"test": creds}}},
+        "py_models": {"credentials_presets": None},
     }
     yaml_data = yaml.dump(json_data, default_flow_style=False)
     with open(siteconfig_path, "w") as file:

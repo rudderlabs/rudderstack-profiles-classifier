712226e8f2bca0004d551cd8ba53f1e04d6ca26e
Subham Subhasis Sahoo <subhamsubhasis2002@gmail.com>
2024-02-13 16:58:12 +0530

    clean up

--- scripts/upload_to_bq.py
+++ scripts/upload_to_bq.py
@@ -1,9 +1,14 @@
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
-def upload_file_to_bigquery(project_id, dataset_id, table_id, file_path, credentials_path):
+
+def upload_file_to_bigquery(
+    project_id, dataset_id, table_id, file_path, credentials_path
+):
     # Load credentials from the provided JSON key file
-    credentials = service_account.Credentials.from_service_account_file(credentials_path)
+    credentials = service_account.Credentials.from_service_account_file(
+        credentials_path
+    )
 
     # Initialize a BigQuery client with the loaded credentials
     client = bigquery.Client(project=project_id, credentials=credentials)
@@ -14,25 +19,22 @@ def upload_file_to_bigquery(project_id, dataset_id, table_id, file_path, credent
     # Get a reference to the table
     table_ref = dataset_ref.table(table_id)
 
-    print(dataset_ref, table_ref)
-
     # Load data into the table from a local file
     with open(file_path, "rb") as source_file:
-        job_config = bigquery.LoadJobConfig(
-            autodetect=True
-        )
+        job_config = bigquery.LoadJobConfig(autodetect=True)
 
         job = client.load_table_from_file(
             source_file,
             table_ref,
             location="US",  # Change this to your desired location
             job_config=job_config,
-        )  
+        )
 
     # Wait for the job to complete
     job.result()
 
     print(f"File {file_path} uploaded to table {table_id} in dataset {dataset_id}.")
+
 
 if __name__ == "__main__":
     # Set your Google Cloud project ID
@@ -49,4 +51,6 @@ if __name__ == "__main__":
     credentials_path = "bqkey.json"
 
     # Call the function to upload the file
-    upload_file_to_bigquery(project_id, dataset_id, table_id, file_path, credentials_path)
+    upload_file_to_bigquery(
+        project_id, dataset_id, table_id, file_path, credentials_path
+    )

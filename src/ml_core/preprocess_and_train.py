73254330aa2a1deee39bfc426210d03cc02c4167
AMBUJ MISHRA <ambuj.mishra.2k12@gmail.com>
2024-02-15 16:08:51 +0530

    figure_name corrections for regression
    Changes to be committed:
    	modified:   src/ml_core/preprocess_and_train.py

--- src/ml_core/preprocess_and_train.py
+++ src/ml_core/preprocess_and_train.py
@@ -339,14 +339,13 @@ if __name__ == "__main__":
     with open(os.path.join(local_folder, args.ec2_temp_output_json), "w") as file:
         json.dump(train_results_json, file)
 
-    train_upload_whitelist = [
-        trainer.figure_names["feature-importance-chart"],
-        trainer.figure_names["lift-chart"],
-        trainer.figure_names["pr-auc-curve"],
-        trainer.figure_names["roc-auc-curve"],
-        f"{trainer.output_profiles_ml_model}_{model_file_name}",
-        args.ec2_temp_output_json,
-    ]
+    train_upload_whitelist = utils.merge_lists_to_unique(
+        list(trainer.figure_names.values()),
+        [
+            f"{trainer.output_profiles_ml_model}_{model_file_name}",
+            args.ec2_temp_output_json,
+        ],
+    )
     if args.mode == constants.K8S_MODE:
         logger.debug(f"Uploading trained files to s3://{args.s3_bucket}/{args.s3_path}")
         S3Utils.upload_directory(

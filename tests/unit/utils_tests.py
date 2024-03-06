fc8a6613980b3627d35bd1881a403d8a6d04cec0
AMBUJ MISHRA <ambuj.mishra.2k12@gmail.com>
2024-03-06 17:05:48 +0530

    changes for bigquery train
    Changes to be committed:
    	modified:   requirements.txt
    	modified:   scripts/simulate_pb_run.py
    	modified:   src/predictions/rudderstack_predictions/connectors/BigQueryConnector.py
    	modified:   src/predictions/rudderstack_predictions/connectors/CommonWarehouseConnector.py
    	modified:   src/predictions/rudderstack_predictions/connectors/Connector.py
    	modified:   src/predictions/rudderstack_predictions/connectors/RedshiftConnector.py
    	modified:   src/predictions/rudderstack_predictions/connectors/SnowflakeConnector.py
    	modified:   src/predictions/rudderstack_predictions/connectors/wh/bigquery_base_connector.py
    	modified:   src/predictions/rudderstack_predictions/connectors/wh/profiles_connector.py
    	modified:   src/predictions/rudderstack_predictions/ml_core/preprocess_and_train.py
    	modified:   src/predictions/rudderstack_predictions/train.py
    	modified:   src/predictions/rudderstack_predictions/utils/utils.py
    	modified:   src/predictions/rudderstack_predictions/wht/rudderPB.py
    	modified:   tests/integration/requirements.txt
    	modified:   tests/unit/utils_tests.py

--- tests/unit/utils_tests.py
+++ tests/unit/utils_tests.py
@@ -18,20 +18,31 @@ class TestReplaceSeqNoInQuery(unittest.TestCase):
     def test_handles_empty_query(self):
         query = ""
         seq_no = 5
-        expected_result = "_5"
-        actual_result = replace_seq_no_in_query(query, seq_no)
-        self.assertEqual(expected_result, actual_result)
+        expected_result = f"Couldn't find an integer seq_no in the input query: {query}"
+        with self.assertRaises(Exception) as context:
+            replace_seq_no_in_query(query, seq_no)
+        self.assertIn(
+            str(context.exception),
+            expected_result,
+        )
 
     def test_handles_missing_seq_no(self):
         query = "SELECT * FROM material_user_var_table_"
         seq_no = 33
-        expected_result = "SELECT * FROM material_user_var_table_33"
+        expected_result = f"Couldn't find an integer seq_no in the input query: {query}"
+        with self.assertRaises(Exception) as context:
+            replace_seq_no_in_query(query, seq_no)
+        self.assertIn(
+            str(context.exception),
+            expected_result,
+        )
+
+    def test_replaces_seq_no_correctly_with_bigquery_input(self):
+        query = "SELECT * FROM `schema`.`material_user_var_table_123`"
+        seq_no = 567
+        expected_result = "SELECT * FROM `schema`.`material_user_var_table_567`"
         actual_result = replace_seq_no_in_query(query, seq_no)
         self.assertEqual(expected_result, actual_result)
-
-    def test_handles_non_string_input(self):
-        with self.assertRaises(AttributeError):
-            replace_seq_no_in_query(123, "456")
 
 
 class TestSplitMaterialTable(unittest.TestCase):
@@ -67,6 +78,24 @@ class TestSplitMaterialTable(unittest.TestCase):
 
     def test_material_query(self):
         table_name = "SELECT * FROM SCHEMA.Material_user_var_table_54ddc22a_383"
+        expected_result = ("user_var_table", "54ddc22a", 383)
+        actual_result = getPB().split_material_table(table_name)
+        self.assertEqual(actual_result, expected_result)
+
+    def test_material_query_with_snowflake_input(self):
+        table_name = "SELECT * FROM Material_user_var_table_54ddc22a_383"
+        expected_result = ("user_var_table", "54ddc22a", 383)
+        actual_result = getPB().split_material_table(table_name)
+        self.assertEqual(actual_result, expected_result)
+
+    def test_material_query_with_redshift_input(self):
+        table_name = "SELECT * FROM material_user_var_table_54ddc22a_383"
+        expected_result = ("user_var_table", "54ddc22a", 383)
+        actual_result = getPB().split_material_table(table_name)
+        self.assertEqual(actual_result, expected_result)
+
+    def test_material_query_with_bigquery_input(self):
+        table_name = "SELECT * FROM `SCHEMA`.`Material_user_var_table_54ddc22a_383`"
         expected_result = ("user_var_table", "54ddc22a", 383)
         actual_result = getPB().split_material_table(table_name)
         self.assertEqual(actual_result, expected_result)

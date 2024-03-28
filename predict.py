93a12bc6e00bd3db56e66fb2bb54c29b31f5dc05
shekhar-rudder <shekhar@rudderstack.com>
2024-03-28 16:17:37 +0530

    refactor: update imports

--- predict.py
+++ predict.py
@@ -1,12 +1,5 @@
-import sys
-import os
-
-sys.path.append(
-    os.path.abspath(os.path.join(os.path.dirname(__file__), "src", "predictions"))
-)
-
-from rudderstack_predictions.predict import _predict
-from rudderstack_predictions.utils import constants
+from src.predictions.rudderstack_predictions.predict import _predict
+from src.predictions.rudderstack_predictions.utils import constants
 
 
 def predict(

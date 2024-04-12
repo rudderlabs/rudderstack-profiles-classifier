7262846f36ac912d904a7f4f6d6f369dddc83a5a
dpatchigolla <dileep.patchigolla@gmail.com>
2024-04-12 17:43:49 +0530

    Handled na error in predict Updated readme to refer to the config file for advanced options

--- src/predictions/rudderstack_predictions/utils/utils.py
+++ src/predictions/rudderstack_predictions/utils/utils.py
@@ -366,9 +366,9 @@ def get_column_names(onehot_encoder: OneHotEncoder, col_names: List[str]) -> Lis
 def transform_null(
     df: pd.DataFrame, numeric_columns: List[str], categorical_columns: List[str]
 ) -> pd.DataFrame:
-    """Replaces the pd.NA values in the numeric and categorical columns of a pandas DataFrame with np.nan and None, respectively."""
     for col in numeric_columns:
         df[col] = df[col].astype("float64")
+    """Replaces the pd.NA values in the numeric and categorical columns of a pandas DataFrame with np.nan and None, respectively."""
     df[numeric_columns] = df[numeric_columns].replace({pd.NA: np.nan})
     df[categorical_columns] = df[categorical_columns].replace({pd.NA: None})
     return df

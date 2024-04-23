232fd74d08473bac73f2499d414672ac950e703a
RuLeKiLleR <bhupendra.rulekiller14@gmail.com>
2024-04-23 14:14:26 +0530

    Made the required changes

--- src/predictions/rudderstack_predictions/py_native/llm.py
+++ src/predictions/rudderstack_predictions/py_native/llm.py
@@ -3,12 +3,9 @@ from profiles_rudderstack.recipe import PyNativeRecipe
 from profiles_rudderstack.material import WhtMaterial
 from profiles_rudderstack.schema import EntityKeyBuildSpecSchema
 from profiles_rudderstack.logger import Logger
-from typing import List
-import re
 
-
-class SnowflakeCortexModel(BaseModelType):
-    TypeName = "sf_cortex"  # the name of the model type
+class LLMModel(BaseModelType):
+    TypeName = "llm_model"  # the name of the model type
 
     # json schema for the build spec
     BuildSpecSchema = {
@@ -16,10 +13,10 @@ class SnowflakeCortexModel(BaseModelType):
         "properties": {
             **EntityKeyBuildSpecSchema["properties"],
             "prompt": {"type": "string"},
-            "vars": {"type": "array", "items": {"type": "string"}},
+            "prompt_inputs": {"type": "array", "items": {"type": "string"}},
             "llm_model_name": {"type": "string"},
         },
-        "required": ["prompt","vars"] + EntityKeyBuildSpecSchema["required"],
+        "required": ["prompt","prompt_inputs"] + EntityKeyBuildSpecSchema["required"],
         "additionalProperties": False,
     }
 
@@ -27,22 +24,18 @@ class SnowflakeCortexModel(BaseModelType):
         super().__init__(build_spec, schema_version, pb_version)
 
     def get_material_recipe(self) -> PyNativeRecipe:
-        return SnowflakeCortexRecipe(
-            self.build_spec["prompt"],
-            self.build_spec["vars"],
-            self.build_spec["llm_model_name"],
-        )
+        return LLMModelRecipe(self.build_spec)
 
     def validate(self):
         return super().validate()
 
 
-class SnowflakeCortexRecipe(PyNativeRecipe):
-    def __init__(self, prompt: str, vars: List[str], llm_model_name: str) -> None:
-        self.prompt = prompt
-        self.vars = vars
-        self.logger = Logger("sf_cortex_recipe")
-        self.llm_model_name = llm_model_name
+class LLMModelRecipe(PyNativeRecipe):
+    def __init__(self, build_spec: dict) -> None:
+        self.prompt = build_spec["prompt"]
+        self.prompt_inputs = build_spec["prompt_inputs"]
+        self.logger = Logger("llm_model_recipe")
+        self.llm_model_name = build_spec["llm_model_name"]
         self.sql = ""
 
     def describe(self, this: WhtMaterial):

5ce38256b13f092d3a01319f6a44c5cd70432135
Srinivas <cnu.1439@gmail.com>
2024-05-08 21:20:28 +0530

    Implementing scikit learn pipeline for data preparation step for predictions

--- src/predictions/rudderstack_predictions/ml_core/preprocess_and_predict.py
+++ src/predictions/rudderstack_predictions/ml_core/preprocess_and_predict.py
@@ -24,10 +24,48 @@ from ..utils.S3Utils import S3Utils
 from ..trainers.MLTrainer import MLTrainer
 from ..connectors.ConnectorFactory import ConnectorFactory
 
+from functools import partial
+from typing import List, Tuple, Dict
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import Pipeline
 from numba.core.errors import NumbaDeprecationWarning, NumbaPendingDeprecationWarning
 
 warnings.filterwarnings("ignore", category=NumbaDeprecationWarning)
 warnings.simplefilter("ignore", category=NumbaPendingDeprecationWarning)
+
+
+def build_data_prep_pipeline(steps: List[Tuple[callable, Dict]]) -> Pipeline:
+    """
+    Builds the data preparation pipeline.
+    """
+    pipeline_steps = []
+    for step in steps:
+        func_name = step[0].__name__
+        transformation_step = FunctionTransformer(
+            func=step[0],
+            kw_args=step[1],
+            validate=False,
+        )
+        pipeline_steps.append((func_name, transformation_step))
+
+    return Pipeline(steps=pipeline_steps)
+
+
+def data_prep_wrapper_func(
+    func: callable, kw_args: Dict, pick_nth_result: int
+) -> callable:
+    """
+    This function is a wrapper around the data preparation function to
+    pick desired return value from the function with multiple return values.
+    """
+
+    func_with_args = partial(func, **kw_args)
+
+    def data_prep_wrapper(data: Any) -> Any:
+        result = func_with_args(data)
+        return result[pick_nth_result]
+
+    return data_prep_wrapper
 
 
 def preprocess_and_predict(
@@ -85,19 +123,32 @@ def preprocess_and_predict(
         seq_no,
     )
 
+    array_type_transformer = data_prep_wrapper_func(
+        connector.transform_arraytype_features,
+        kw_args={"arraytype_features": arraytype_columns},
+        pick_nth_result=1,
+    )
+
+    data_prep_pipeline = build_data_prep_pipeline(
+        [
+            (
+                connector.add_days_diff,
+                {"new_col": col, "time_col": col, "end_ts": end_ts},
+            )
+            for col in timestamp_columns
+        ]
+        + [
+            (array_type_transformer, {}),
+            (connector.drop_cols, {"col_list": ignore_features}),
+        ]
+    )
+
     logger.debug(f"Pulling data from Entity-Var table - {entity_var_table_name}")
     raw_data = connector.get_table(
         entity_var_table_name, filter_condition=trainer.eligible_users
     )
 
-    logger.debug("Transforming timestamp columns.")
-    for col in timestamp_columns:
-        raw_data = connector.add_days_diff(raw_data, col, col, end_ts)
-
-    logger.debug(f"Transforming arraytype columns.")
-    _, raw_data = connector.transform_arraytype_features(raw_data, arraytype_columns)
-
-    predict_data = connector.drop_cols(raw_data, ignore_features)
+    predict_data = data_prep_pipeline.fit_transform(raw_data)
 
     required_features_upper_case = set(
         [

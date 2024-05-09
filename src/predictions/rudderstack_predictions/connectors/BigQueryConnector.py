c4fec573aa5309b450ca271769518d3b85c1c26a
chetana <chetanatanmayee@gmail.com>
2024-05-10 01:16:10 +0530

    added 3 functions to support bigframes df

--- src/predictions/rudderstack_predictions/connectors/BigQueryConnector.py
+++ src/predictions/rudderstack_predictions/connectors/BigQueryConnector.py
@@ -1,5 +1,7 @@
 import json
 import pandas as pd
+import bigframes
+import bigframes.pandas as bpd
 from typing import List, Tuple, Optional
 
 import google.cloud
@@ -30,6 +32,7 @@ class BigQueryConnector(CommonWarehouseConnector):
             "arraytype": ("ARRAY",),
             "booleantype": ("BOOLEAN", "BOOL"),
         }
+
         super().__init__(creds, folder_path, data_type_mapping)
 
     def build_session(self, credentials: dict) -> google.cloud.bigquery.client.Client:
@@ -46,6 +49,7 @@ class BigQueryConnector(CommonWarehouseConnector):
                 default_dataset=f"{credentials['project_id']}.{credentials['schema']}"
             ),
         )
+        bpd.options.bigquery.project = self.project_id
         return session
 
     def run_query(self, query: str, response=True) -> Optional[List]:
@@ -62,6 +66,50 @@ class BigQueryConnector(CommonWarehouseConnector):
         except Exception as e:
             raise Exception(f"Couldn't run the query: {query}. Error: {str(e)}")
 
+    ## Functions added by me
+    def _create_get_table_query(self, table_name, **kwargs):
+        filter_condition = kwargs.get("filter_condition", "")
+        query = f"SELECT * FROM {self.project_id}.{self.schema}.{table_name}"
+        if filter_condition:
+            query += f" WHERE {filter_condition}"
+        query += ";"
+        return query
+
+    def get_material_registry_table(
+        self,
+        material_registry_table_name: str,
+    ) -> pd.DataFrame:
+        """Fetches and filters the material registry table to get only the successful runs. It assumes that the successful runs have a status of 2.
+        Currently profiles creates a row at the start of a run with status 1 and creates a new row with status to 2 at the end of the run.
+        """
+        material_registry_table = self.get_table(material_registry_table_name)
+
+        def safe_parse_json(entry):
+            try:
+                if isinstance(
+                    entry, str
+                ):  # If the entry is a string(for redshift), parse it as JSON
+                    entry_dict = eval(entry)
+                elif isinstance(
+                    entry, dict
+                ):  # If the entry is already a dictionary(for bigquery), use it directly
+                    entry_dict = entry
+
+                return entry_dict.get("complete", {}).get("status")
+            except:
+                return None
+
+        material_registry_table["status"] = material_registry_table["metadata"].apply(
+            safe_parse_json, by_row=False
+        )
+        return material_registry_table[material_registry_table["status"] == 2]
+
+    def get_table(self, table_name: str, **kwargs) -> bigframes.dataframe.DataFrame:
+        """Fetches the table with the given name from the schema as a pandas Dataframe object."""
+        query = self._create_get_table_query(table_name, **kwargs)
+        return bpd.read_gbq(query)
+
+    ############################
     def get_table_as_dataframe(
         self, _: google.cloud.bigquery.client.Client, table_name: str, **kwargs
     ) -> pd.DataFrame:

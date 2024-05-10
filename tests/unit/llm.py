01033ac43cd7fec4ca3e85012ba107ff0831d6ca
RuLeKiLleR <bhupendra.rulekiller14@gmail.com>
2024-05-10 15:38:21 +0530

    Format corrected

--- tests/unit/llm.py
+++ tests/unit/llm.py
@@ -7,18 +7,18 @@ class TestLLMModelValidation(unittest.TestCase):
     build_spec = {
         "prompt": "sample prompt input{[0]}",
         "prompt_inputs": ["input1", "input2"],
-        "llm_model_name": None
+        "llm_model_name": None,
     }
     schema_ver = 53
     pb_version = "v0.11.2"
 
-    @patch('re')
+    @patch("re")
     def test_llm_model_name(self, mock_re):
         # Defining Schema
         build_spec = {
             "prompt": "sample prompt input{[0]}",
             "prompt_inputs": ["input1", "input2"],
-            "llm_model_name": None
+            "llm_model_name": None,
         }
         # Testing empty model name
         llm_model = LLMModel(build_spec, self.schema_ver, self.pb_version)
@@ -28,16 +28,19 @@ class TestLLMModelValidation(unittest.TestCase):
         default_llm_model_name = "llama2-70b-chat"
         self.assertEqual(default_llm_model_name, build_spec["llm_model_name"])
         # Testing invalid model name
-        build_spec['llm_model_name'] = "invalid_model"
+        build_spec["llm_model_name"] = "invalid_model"
         llm_model = LLMModel(build_spec, self.schema_ver, self.pb_version)
         with self.assertRaises(ValueError):
             llm_model.validate()
 
-    @patch('re')
+    @patch("re")
     def test_max_index_prompt_inputs(self, mock_re):
         # Mocking LLMModel class
         llm_model = LLMModel(self.build_spec, 53, "v0.11.2")
-        input_indices = mock_re.findall.return_value = [("input1", "0"), ("input2", "1")]
+        input_indices = mock_re.findall.return_value = [
+            ("input1", "0"),
+            ("input2", "1"),
+        ]
         max_index_direct = len(self.build_spec["prompt_inputs"]) - 1
         # Validate the SnowflakeCortexModel
         llm_model.validate()
@@ -49,7 +52,7 @@ class TestLLMModelValidation(unittest.TestCase):
         # Assert
         self.assertTrue(are_equal)
         # Testing the case when prompt maximum index is greater then the size of prompt_inputs
-        self.build_spec['prompt'] = "sample prompt {input1[2]} {input2[3]}"
+        self.build_spec["prompt"] = "sample prompt {input1[2]} {input2[3]}"
         llm_model = LLMModel(self.build_spec, 53, "v0.11.2")
         mock_re.findall.return_value = [("input1", "2"), ("input2", "3")]
         with self.assertRaises(ValueError):

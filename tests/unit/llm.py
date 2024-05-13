0df8c1e8f4905db1ca3e4a017ab8285967783123
RuLeKiLleR <bhupendra.rulekiller14@gmail.com>
2024-05-13 11:21:05 +0530

    Format corrected

--- tests/unit/llm.py
+++ tests/unit/llm.py
@@ -7,7 +7,7 @@ class TestLLMModelValidation(unittest.TestCase):
         self.build_spec = {
             "prompt": "sample prompt input{[0]}",
             "prompt_inputs": ["input1", "input2"],
-            "llm_model_name": None
+            "llm_model_name": None,
         }
         self.schema_ver = 53
         self.pb_version = "v0.11.2"
@@ -22,7 +22,7 @@ class TestLLMModelValidation(unittest.TestCase):
         self.assertEqual(default_llm_model_name, self.build_spec["llm_model_name"])
 
         # Testing invalid model name
-        self.build_spec['llm_model_name'] = "invalid_model"
+        self.build_spec["llm_model_name"] = "invalid_model"
         llm_model = LLMModel(self.build_spec, self.schema_ver, self.pb_version)
         with self.assertRaises(ValueError):
             llm_model.validate()
@@ -30,17 +30,17 @@ class TestLLMModelValidation(unittest.TestCase):
     def test_max_index_prompt_inputs(self):
         # Testing max index prompt inputs
         # First, test with valid indices
-        self.build_spec['prompt'] = "sample prompt"
+        self.build_spec["prompt"] = "sample prompt"
         llm_model = LLMModel(self.build_spec, self.schema_ver, self.pb_version)
         llm_model.validate()
 
         # Testing the case for correct maximum index
-        self.build_spec['prompt'] = "sample prompt {input1[0]} {input2[1]}"
+        self.build_spec["prompt"] = "sample prompt {input1[0]} {input2[1]}"
         llm_model = LLMModel(self.build_spec, self.schema_ver, self.pb_version)
         llm_model.validate()
 
         # Now, test with invalid indices
-        self.build_spec['prompt'] = "sample prompt {input1[2]} {input2[3]}"
+        self.build_spec["prompt"] = "sample prompt {input1[2]} {input2[3]}"
         llm_model = LLMModel(self.build_spec, self.schema_ver, self.pb_version)
         with self.assertRaises(ValueError):
             llm_model.validate()
@@ -48,10 +48,13 @@ class TestLLMModelValidation(unittest.TestCase):
     def test_prompt_length_validation(self):
         # Testing prompt length validation
         # Set a large prompt that exceeds the token limit
-        self.build_spec["prompt"] = "a " * 40000  # Assuming the token limit for "llama2-70b-chat" is 4096
+        self.build_spec["prompt"] = (
+            "a " * 40000
+        )  # Assuming the token limit for "llama2-70b-chat" is 4096
 
         # Ensure that ValueError is raised due to prompt length exceeding the limit
         with self.assertRaises(ValueError):
-            snowflake_cortex_model = LLMModel(self.build_spec, self.schema_ver, self.pb_version)
+            snowflake_cortex_model = LLMModel(
+                self.build_spec, self.schema_ver, self.pb_version
+            )
             snowflake_cortex_model.validate()
-

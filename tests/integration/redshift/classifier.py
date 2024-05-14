d00920a61b58c4407ca4ef2a5119597a78ce80cb
Subham Subhasis Sahoo <72215169+subham-71@users.noreply.github.com>
2024-05-14 17:32:59 +0530

    updated integration test (#290)
    * updated integration test
    
    * Update classifier.py
    
    * Update regressor.py
    
    * updated creds config
    
    * updated schema

--- tests/integration/redshift/classifier.py
+++ tests/integration/redshift/classifier.py
@@ -8,7 +8,7 @@ import os
 
 
 creds = json.loads(os.environ["REDSHIFT_SITE_CONFIG"])
-creds["schema"] = "rs_profiles_3"
+creds["schema"] = "profiles_new1"
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_path = os.path.join(current_dir, "sample_project")

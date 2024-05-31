6f93e08787b796f520b2a8510d63679e1701e62d
Bhupendra Singh Chauhan <79101171+RuLeKiLLeR@users.noreply.github.com>
2024-05-31 14:55:19 +0530

    Changes in the code to fetch details of the custom table (#336)

--- src/predictions/profiles_mlcorelib/py_native/llm.py
+++ src/predictions/profiles_mlcorelib/py_native/llm.py
@@ -1,7 +1,10 @@
 from profiles_rudderstack.model import BaseModelType
 from profiles_rudderstack.recipe import PyNativeRecipe
 from profiles_rudderstack.material import WhtMaterial
-from profiles_rudderstack.schema import EntityKeyBuildSpecSchema
+from profiles_rudderstack.schema import (
+    EntityKeyBuildSpecSchema,
+    EntityIdsBuildSpecSchema,
+)
 from profiles_rudderstack.logger import Logger
 import re
 
@@ -14,24 +17,47 @@ class LLMModel(BaseModelType):
         "type": "object",
         "properties": {
             **EntityKeyBuildSpecSchema["properties"],
+            **EntityIdsBuildSpecSchema["properties"],
             "prompt": {"type": "string"},
-            "var_inputs": {"type": "array", "items": {"type": "string"}},
+            "var_inputs": {"type": ["array", "null"], "items": {"type": "string"}},
+            "table_inputs": {
+                "type": ["array", "null"],
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "select": {"type": "string"},
+                        "from": {"type": "string"},
+                    },
+                    "required": ["select", "from"],
+                },
+            },
             "sql_inputs": {"type": "array", "items": {"type": "string"}},
             "llm_model_name": {"type": ["string", "null"]},
-            "run_for_top_k_distinct": {"type": "integer"},
+            "run_for_top_k_distinct": {"type": ["integer", "null"]},
         },
-        "required": ["prompt", "var_inputs"] + EntityKeyBuildSpecSchema["required"],
+        "required": ["prompt"] + EntityKeyBuildSpecSchema["required"],
+        "oneOf": [{"required": ["var_inputs"]}, {"required": ["table_inputs"]}],
         "additionalProperties": False,
     }
     DEFAULT_SQL_INPUTS = []
     DEFAULT_LLM_MODEL = "llama2-70b-chat"
 
     def __init__(self, build_spec: dict, schema_version: int, pb_version: str) -> None:
+        if "ids" not in build_spec:
+            entity_key = build_spec["entity_key"]
+            # TODO - select should be computed from the entity object
+            build_spec["ids"] = [
+                {
+                    "select": entity_key + "_main_id",
+                    "type": "rudder_id",
+                    "entity": entity_key,
+                }
+            ]
+        if "llm_model_name" not in build_spec:
+            build_spec["llm_model_name"] = self.DEFAULT_LLM_MODEL
         super().__init__(build_spec, schema_version, pb_version)
         if "sql_inputs" not in self.build_spec:
             self.build_spec["sql_inputs"] = self.DEFAULT_SQL_INPUTS
-        if self.build_spec["llm_model_name"] == None:
-            self.build_spec["llm_model_name"] = self.DEFAULT_LLM_MODEL
 
     def get_material_recipe(self) -> PyNativeRecipe:
         return LLMModelRecipe(self.build_spec)
@@ -51,10 +77,14 @@ class LLMModel(BaseModelType):
         }
 
         prompt = self.build_spec["prompt"]
-        var_input_lst = self.build_spec["var_inputs"]
+        if "var_inputs" in self.build_spec:
+            var_inputs = self.build_spec["var_inputs"]
+            table_inputs = None
+        else:
+            var_inputs = None
+            table_inputs = self.build_spec["table_inputs"]
         sql_inputs_lst = self.build_spec["sql_inputs"]
         model_name = self.build_spec["llm_model_name"]
-
         prompt_tokens = len(prompt.split())
         if model_name in model_limits:
             if prompt_tokens > model_limits[model_name]:
@@ -74,48 +104,67 @@ class LLMModel(BaseModelType):
                 default=-1,
             )
 
-        if max_var_inputs_index >= len(var_input_lst):
+        validation_inputs = var_inputs if var_inputs is not None else table_inputs
+
+        if validation_inputs is not None and max_var_inputs_index >= len(
+            validation_inputs
+        ):
             raise ValueError(
-                f"Maximum index {max_var_inputs_index} is out of range for var_inputs list."
+                f"Maximum index {max_var_inputs_index} is out of range for the inputs list."
             )
+
         if max_sql_inputs_index >= len(sql_inputs_lst):
             raise ValueError(
                 f"Maximum index {max_sql_inputs_index} is out of range for sql_inputs list."
             )
+        if table_inputs:
+            from_tables = set()
+            for var in table_inputs:
+                table_name = var.get("from")
+                from_tables.add(table_name)
+            if len(from_tables) > 1:
+                raise ValueError(
+                    "The model doesn't support specifying more than 1 input tables"
+                )
+
         return super().validate()
 
 
 class LLMModelRecipe(PyNativeRecipe):
     def __init__(self, build_spec: dict) -> None:
         self.prompt = build_spec["prompt"]
-        self.var_inputs = build_spec["var_inputs"]
-        self.sql_inputs = build_spec["sql_inputs"]
+        if "var_inputs" in build_spec:
+            self.var_inputs = build_spec["var_inputs"]
+            self.table_inputs = None
+        else:
+            self.var_inputs = None
+            self.table_inputs = build_spec["table_inputs"]
         self.logger = Logger("llm_model_recipe")
         self.llm_model_name = build_spec["llm_model_name"]
-        self.k_distinct_values = build_spec["run_for_top_k_distinct"]
+        self.k_distinct_values = (
+            build_spec["run_for_top_k_distinct"]
+            if "run_for_top_k_distinct" in build_spec
+            else None
+        )
+        self.sql_inputs = build_spec["sql_inputs"]
         self.sql = ""
 
     def describe(self, this: WhtMaterial):
         return self.sql, ".sql"
 
-    def register_dependencies(self, this: WhtMaterial):
-        entity = this.model.entity()
-        column_name = this.model.name()
-        if entity is None:
-            raise Exception("no entity found for model")
-        input_columns = [
-            f"{{{{{var}.Model.DbObjectNamePrefix()}}}}"  # Assuming var is already in the format "user.Var('profession')"
-            for var in self.var_inputs
-        ]
-        sql_inputs_df = None
-        if not this.wht_ctx.is_null_ctx and len(self.sql_inputs) > 0:
-            sql_inputs_df = [
-                this.wht_ctx.client.query_sql_with_result(sql_query).to_json(
-                    orient="records"
-                )
-                for sql_query in self.sql_inputs
-            ]
-        entity_id_column_name = this.model.entity().get("IdColumnName")
+    def query_template_creator(
+        self,
+        input_material_template,
+        column_name,
+        entity_id_column_name,
+        input_columns_vars,
+        sql_inputs_df,
+    ):
+        limit_top_k = (
+            f"ORDER BY frequency DESC LIMIT {self.k_distinct_values}"
+            if self.k_distinct_values
+            else ""
+        )
         prompt_replaced = self.prompt.replace(
             "'", "''", -1
         )  # This is to escape single quotes
@@ -125,32 +174,23 @@ class LLMModelRecipe(PyNativeRecipe):
             placeholder = f"{{{word}[{index}]}}"
             replacement_prompt = ""
             if word == "var_inputs":
-                replacement_prompt = "' ||" + input_columns[index] + "|| ' "
+                replacement_prompt = "' ||" + input_columns_vars[index] + "|| ' "
             elif word == "sql_inputs" and sql_inputs_df:
                 replacement_prompt = sql_inputs_df[index]
             prompt_replaced = prompt_replaced.replace(placeholder, replacement_prompt)
-
-        var_table_ref = (
-            f"this.DeRef(makePath({self.var_inputs[0]}.Model.GetVarTableRef()))"
-        )
         # Joined_columns used to create a comma seperated string in order to mention
         # all the columns that are used as input in the query.
-        joined_columns = ", ".join(input_columns)
+        joined_columns = ", ".join(input_columns_vars)
         # Join condition to join the predicted column to the original table in order to mention
         # all the column that are being used in the input columns list.
-        join_condition = " AND ".join([f"a.{col} = b.{col}" for col in input_columns])
-
-        limit_top_k = (
-            f"ORDER BY frequency DESC LIMIT {self.k_distinct_values}"
-            if self.k_distinct_values
-            else ""
+        join_condition = " AND ".join(
+            [f"a.{col} = b.{col}" for col in input_columns_vars]
         )
-
         # model_creator_sql
         query_template = f"""
             {{% macro begin_block() %}}
                 {{% macro selector_sql() %}}
-                    {{% set entityVarTable = {var_table_ref} %}}
+                    {{% set entityVarTable = {input_material_template} %}}
                     -- Common Table Expression (CTE) to get the distinct values of specified columns based on their frequency
                     WITH top_k_distinct_attribute AS (
                         SELECT {joined_columns}, COUNT(*) AS frequency
@@ -164,15 +204,71 @@ class LLMModelRecipe(PyNativeRecipe):
                     )
                     SELECT a.{entity_id_column_name}, b.{column_name}
                     FROM {{{{entityVarTable}}}} a
-                    -- Perform a LEFT JOIN between the original table and the predicted attributes to fill all the 
+                    -- Perform a LEFT JOIN between the original table and the predicted attributes to fill all the
                     -- attribute values with their corresponding predicted value.
                     LEFT JOIN predicted_attribute b ON {join_condition}
                 {{% endmacro %}}
                 {{% exec %}} {{{{warehouse.CreateReplaceTableAs(this.Name(), selector_sql())}}}} {{% endexec %}}
             {{% endmacro %}}
             {{% exec %}} {{{{warehouse.BeginEndBlock(begin_block())}}}} {{% endexec %}}"""
+        return query_template
 
-        self.sql = this.execute_text_template(query_template)
+    def register_dependencies(self, this: WhtMaterial):
+        entity_id_column_name = this.model.entity().get("IdColumnName")
+        column_name = this.model.name()
+        sql_inputs_df = None
+        if not this.wht_ctx.is_null_ctx and len(self.sql_inputs) > 0:
+            sql_inputs_df = [
+                this.wht_ctx.client.query_sql_with_result(sql_query).to_json(
+                    orient="records"
+                )
+                for sql_query in self.sql_inputs
+            ]
+
+        if self.var_inputs:
+            var_table_material_template = (
+                f"this.DeRef(makePath({self.var_inputs[0]}.Model.GetVarTableRef()))"
+            )
+            entity = this.model.entity()
+            if entity is None:
+                raise Exception("no entity found for model")
+            input_columns_vars = [
+                f"{{{{{var}.Model.DbObjectNamePrefix()}}}}"  # Assuming var is already in the format "user.Var('profession')"
+                for var in self.var_inputs
+            ]
+
+            query_template = LLMModelRecipe.query_template_creator(
+                self,
+                var_table_material_template,
+                column_name,
+                entity_id_column_name,
+                input_columns_vars,
+                sql_inputs_df,
+            )
+            self.sql = this.execute_text_template(query_template)
+
+        elif self.table_inputs:
+            table_columns = []
+            for var in self.table_inputs:
+                input_column_name = var.get("select")
+                table_columns.append(input_column_name)
+
+            table_name = self.table_inputs[0].get("from")
+            entity_id_column_material = this.de_ref(
+                f"{table_name}/var_table/{entity_id_column_name}"
+            )
+            var_table_material_template = f"this.DeRef('{f'{table_name}/var_table'}')"
+
+            query_template = LLMModelRecipe.query_template_creator(
+                self,
+                var_table_material_template,
+                column_name,
+                entity_id_column_name,
+                table_columns,
+                sql_inputs_df,
+            )
+            self.sql = this.execute_text_template(query_template)
+
         return
 
     def execute(self, this: WhtMaterial):

990b88d6f18987ce9e162e232533e56bf4d1ee0d
AMBUJ MISHRA <ambuj.mishra.2k12@gmail.com>
2024-06-10 20:48:08 +0530

    dummy integration test

--- tests/integration/bigquery/classifier.py
+++ tests/integration/bigquery/classifier.py
@@ -177,7 +177,7 @@ def test_classification():
     et = time.time()
     # get the execution time
     elapsed_time = et - st
-    print("Execution time:", elapsed_time, "seconds")
+    print("Execution time is:", elapsed_time, "seconds")
 
 
 test_classification()

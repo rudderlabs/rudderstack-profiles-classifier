3f52672033f965c65bcefb6cb03d476078914b82
shekhar-rudder <shekhar@rudderstack.com>
2024-07-04 12:06:29 +0530

    fix: unable to split material name for quoted input

--- tests/unit/utils_tests.py
+++ tests/unit/utils_tests.py
@@ -14,11 +14,32 @@ from src.predictions.profiles_mlcorelib.utils.utils import (
 
 class TestReplaceSeqNoInQuery(unittest.TestCase):
     def test_replaces_seq_no_correctly(self):
-        query = "SELECT * FROM material_user_var_table_123"
-        seq_no = 567
-        expected_result = "SELECT * FROM material_user_var_table_567"
-        actual_result = replace_seq_no_in_query(query, seq_no)
-        self.assertEqual(expected_result, actual_result)
+        test_cases = [
+            {
+                "input": "SELECT * FROM material_user_var_table_123",
+                "output": "SELECT * FROM material_user_var_table_567",
+            },
+            {
+                "input": "SELECT * FROM `schema`.`material_user_var_table_123`",
+                "output": "SELECT * FROM `schema`.`material_user_var_table_567`",
+            },
+            {
+                "input": '''SELECT days_since_last_seen FROM "rs_profiles_3"."material_user_var_table_54ddc22a_383"''',
+                "output": '''SELECT days_since_last_seen FROM "rs_profiles_3"."material_user_var_table_54ddc22a_567"''',
+            },
+            {
+                "input": "SELECT days_since_last_seen FROM 'rs_profiles_3'.'material_user_var_table_54ddc22a_383'",
+                "output": "SELECT days_since_last_seen FROM 'rs_profiles_3'.'material_user_var_table_54ddc22a_567'",
+            },
+            {
+                "input": 'SELECT days_since_last_seen FROM "rs_profiles_3"."material_user_var_table_54ddc22a_383"',
+                "output": 'SELECT days_since_last_seen FROM "rs_profiles_3"."material_user_var_table_54ddc22a_567"',
+            },
+        ]
+        for case in test_cases:
+            with self.subTest(case=case["input"]):
+                result = replace_seq_no_in_query(case["input"], 567)
+                self.assertEqual(result, case["output"])
 
     def test_handles_empty_query(self):
         query = ""
@@ -42,34 +63,30 @@ class TestReplaceSeqNoInQuery(unittest.TestCase):
             expected_result,
         )
 
-    def test_replaces_seq_no_correctly_with_bigquery_input(self):
-        query = "SELECT * FROM `schema`.`material_user_var_table_123`"
-        seq_no = 567
-        expected_result = "SELECT * FROM `schema`.`material_user_var_table_567`"
-        actual_result = replace_seq_no_in_query(query, seq_no)
-        self.assertEqual(expected_result, actual_result)
-
 
 class TestSplitMaterialTable(unittest.TestCase):
     def test_valid_table_name(self):
-        table_name = "Material_user_var_table_54ddc22a_383"
+        test_cases = [
+            "Material_user_var_table_54ddc22a_383",
+            "user_var_table_54ddc22a_383",
+            "SELECT * FROM SCHEMA.Material_user_var_table_54ddc22a_383",
+            "SELECT * FROM `SCHEMA.Material_user_var_table_54ddc22a_383`",
+            "SELECT * FROM Material_user_var_table_54ddc22a_383",
+            "SELECT * FROM material_user_var_table_54ddc22a_383",  # redshift_input
+            "SELECT * FROM `SCHEMA`.`Material_user_var_table_54ddc22a_383`",  # bigquery_input
+            'SELECT days_since_last_seen FROM "rs_profiles_3"."material_user_var_table_54ddc22a_383"'
+            '''SELECT days_since_last_seen FROM "rs_profiles_3"."material_user_var_table_54ddc22a_383"''',
+            "SELECT days_since_last_seen FROM 'rs_profiles_3'.'material_user_var_table_54ddc22a_383'",
+        ]
         expected_result = {
             "model_name": "user_var_table",
             "model_hash": "54ddc22a",
             "seq_no": 383,
         }
-        actual_result = PythonWHT().split_material_name(table_name)
-        self.assertEqual(actual_result, expected_result)
-
-    def test_missing_prefix(self):
-        table_name = "user_var_table_54ddc22a_383"
-        expected_result = {
-            "model_name": "user_var_table",
-            "model_hash": "54ddc22a",
-            "seq_no": 383,
-        }
-        actual_result = PythonWHT().split_material_name(table_name)
-        self.assertEqual(actual_result, expected_result)
+        for case in test_cases:
+            with self.subTest(case=case):
+                result = PythonWHT().split_material_name(case)
+                self.assertEqual(result, expected_result)
 
     def test_missing_seq_no(self):
         table_name = "Material_user_var_table_54ddc22a"
@@ -85,46 +102,6 @@ class TestSplitMaterialTable(unittest.TestCase):
         table_name = "user_var_table_54ddc22a_foo"
         with self.assertRaises(Exception):
             PythonWHT().split_material_name(table_name)
-
-    def test_material_query(self):
-        table_name = "SELECT * FROM SCHEMA.Material_user_var_table_54ddc22a_383"
-        expected_result = {
-            "model_name": "user_var_table",
-            "model_hash": "54ddc22a",
-            "seq_no": 383,
-        }
-        actual_result = PythonWHT().split_material_name(table_name)
-        self.assertEqual(actual_result, expected_result)
-
-    def test_material_query_with_snowflake_input(self):
-        table_name = "SELECT * FROM Material_user_var_table_54ddc22a_383"
-        expected_result = {
-            "model_name": "user_var_table",
-            "model_hash": "54ddc22a",
-            "seq_no": 383,
-        }
-        actual_result = PythonWHT().split_material_name(table_name)
-        self.assertEqual(actual_result, expected_result)
-
-    def test_material_query_with_redshift_input(self):
-        table_name = "SELECT * FROM material_user_var_table_54ddc22a_383"
-        expected_result = {
-            "model_name": "user_var_table",
-            "model_hash": "54ddc22a",
-            "seq_no": 383,
-        }
-        actual_result = PythonWHT().split_material_name(table_name)
-        self.assertEqual(actual_result, expected_result)
-
-    def test_material_query_with_bigquery_input(self):
-        table_name = "SELECT * FROM `SCHEMA`.`Material_user_var_table_54ddc22a_383`"
-        expected_result = {
-            "model_name": "user_var_table",
-            "model_hash": "54ddc22a",
-            "seq_no": 383,
-        }
-        actual_result = PythonWHT().split_material_name(table_name)
-        self.assertEqual(actual_result, expected_result)
 
 
 class TestDateDiff(unittest.TestCase):

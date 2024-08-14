59e24724a1eb5bbf49831b4931cc97cbb18f0cae
RuLeKiLleR <bhupendra.rulekiller14@gmail.com>
2024-08-15 00:30:15 +0530

    Removed redundancy in the de_ref function in register_dependencies

--- src/predictions/profiles_mlcorelib/py_native/training.py
+++ src/predictions/profiles_mlcorelib/py_native/training.py
@@ -178,7 +178,10 @@ class TrainingRecipe(PyNativeRecipe):
 
     def register_dependencies(self, this: WhtMaterial):
         for input in self.build_spec["inputs"]:
-            this.de_ref(input)
+            if input == self.build_spec["ml_config"]["data"]["label_column"]:
+                this.de_ref(self.build_spec["ml_config"]["data"]["label_column"])
+            else:
+                this.de_ref(input)
 
     def get_training_file_path(this: WhtMaterial):
         folder = this.get_output_folder()

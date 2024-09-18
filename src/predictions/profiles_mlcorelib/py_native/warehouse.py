3275886a33357e95659b0f7ce557768bee215246
shekhar-rudder <shekhar@rudderstack.com>
2024-09-18 22:32:55 +0530

    refactor: warehouse

--- src/predictions/profiles_mlcorelib/py_native/warehouse.py
+++ src/predictions/profiles_mlcorelib/py_native/warehouse.py
@@ -1,9 +1,9 @@
-def standardize_ref_name(warehouse_type: str, ref_name: str):
+def standardize_ref_name(warehouse_type: str, ref_name1: str):
     # This is the equivalent of the StandardizeRefName function in wht.
     # It is not being used by most of pynative models since they use "warehouse.CreateReplaceTableAs"
     # which has this logic built in.
     if warehouse_type == "snowflake":
-        return ref_name.upper()
+        return ref_name1.upper()
     if warehouse_type == "redshift":
-        return ref_name.lower()
-    return ref_name
+        return ref_name1.lower()
+    return ref_name1

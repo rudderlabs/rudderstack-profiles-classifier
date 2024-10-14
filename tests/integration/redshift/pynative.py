25f423e81a8f004a6ccc229f3139f06451b06a82
chetana <chetanatanmayee@gmail.com>
2024-10-14 11:29:46 +0530

    formatted

--- tests/integration/redshift/pynative.py
+++ tests/integration/redshift/pynative.py
@@ -29,8 +29,7 @@ def run_project():
     finally:
         cleanup_pb_project(project_path, siteconfig_path)
         cleanup_args = pb_cleanup_warehouse_tables(project_path, siteconfig_path)
-        subprocess.run(cleanup_args) 
-
+        subprocess.run(cleanup_args)
 
 
 run_project()
